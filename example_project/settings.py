7357569742268df53d811d29475e2cabb38b24a5
Ivan Šimičić <ivan@vumedi.com>
2019-05-17 13:49:03 +0200

    Support for django 1.11.

--- example_project/settings.py
+++ example_project/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = (
     'example_project',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
